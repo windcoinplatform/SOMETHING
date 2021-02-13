484234e0dfb6eb0bc7dbaa91619fd66d99bbe7e5
WIND <57537825+windcoinplatform@users.noreply.github.com>
2021-02-13 17:23:38 +0300

    Update CurrencyService.js

--- src/js/services/CurrencyService.js
+++ src/js/services/CurrencyService.js
@@ -27,7 +27,7 @@ export class CurrencyService extends ApiClientService {
 
     get = assetId => {
         if (!assetId) {
-            return Promise.resolve(Currency.WIND);
+            return Promise.resolve(Currency.SOMETHING);
         }
 
         // TODO: remove after token is renamed
