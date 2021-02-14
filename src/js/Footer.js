33c033621eea57702ac1f91da23834335ee3ec7b
WIND <57537825+windcoinplatform@users.noreply.github.com>
2021-02-14 13:28:19 +0300

    Update Footer.js

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
+const socialLinks = [{
+    id: 'github',
+    url: 'http://161.97.178.27:8080/custom'
+}, {
+    id: 'twitter',
+    url: 'http://161.97.178.27:8080/custom'
+}, {
+    id: 'facebook',
+    url: 'http://161.97.178.27:8080/custom'
+}, {
+    id: 'discord',
+    url: 'http://161.97.178.27:8080/custom'
+}, {
+    id: 'telegram',
+    url: 'http://161.97.178.27:8080/custom'
+}, {
+    id: 'reddit',
+    url: 'http://161.97.178.27:8080/custom'
+}];
 
+const Footer = ({version}) => {
+    return (
         <div className="menu-footer">
             <div>Version</div>
             <div>SOMETHING Team</div>
@@ -12,5 +33,7 @@ import React from 'react';
                 <a className="fade" href="http://161.97.178.27:8080/custom" target="_blank">SOMETHING</a>
             </div>
         </div>
- 
+    );
+}
+
 export default Footer;
