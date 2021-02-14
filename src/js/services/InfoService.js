cffd0526603fe5e22c8ad8ca70ae6f5e8877010b
WIND <57537825+windcoinplatform@users.noreply.github.com>
2021-02-14 12:52:34 +0300

    Update InfoService.js

--- src/js/services/InfoService.js
+++ src/js/services/InfoService.js
@@ -32,7 +32,7 @@ export class InfoService extends ApiClientService {
             api.baseTarget()
         ]).then(axios.spread((version, height, baseTarget) => {
             return {
-                [CAPTIONS.VERSION]: " SKS 0.1.1",
+                [CAPTIONS.VERSION]: " KSS 0.1.1",
                 [CAPTIONS.CURRENT_HEIGHT]: height,
                 [CAPTIONS.BASE_TARGET]: baseTarget.data.baseTarget
             };
